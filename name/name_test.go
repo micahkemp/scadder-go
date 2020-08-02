dd1704bf510f19d192b1648a46a0f083a24a1d00
Micah Kemp <apple@micah.ws>
2020-08-02 14:05:43 -0500

    Consolidated name tests.

--- name/name_test.go
+++ name/name_test.go
@@ -2,6 +2,31 @@ package name
 
 import "testing"
 
+func TestValid(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"", false},
+		{"a", false},
+		{"0a", false},
+		{"a0", false},
+		{"a-a", false},
+		{"_a", false},
+		{"a_", false},
+		{"a a", false},
+		{"a0a", true},
+		{"aa", true},
+		{"a_a", true},
+	}
+
+	for _, test := range tests {
+		if got := valid(test.input); got != test.want {
+			t.Errorf("Valid(%q) = %v", test.input, got)
+		}
+	}
+}
+
 func TestFirstValid(t *testing.T) {
 	tests := []struct {
 		choices  []string
@@ -20,6 +45,28 @@ func TestFirstValid(t *testing.T) {
 				ok,
 				test.wantName,
 				test.wantOk)
+		}
+	}
+}
+
+func TestFilename(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"aa", "aa.scad"},
+		{"bb", "bb.scad"},
+	}
+
+	for _, test := range tests {
+		n, _ := NewName(test.input)
+		got := n.Filename()
+
+		if got != test.want {
+			t.Errorf("NewName(%q).Filename() = %q, want %q",
+				test.input,
+				got,
+				test.want)
 		}
 	}
 }
