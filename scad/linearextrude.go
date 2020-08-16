4d93d21a2b98dc54359b84319667e83d5528eeaa
Micah Kemp <apple@micah.ws>
2020-08-16 14:53:13 -0500

    Name is the first parameter to NewLinearExtrude.

--- scad/linearextrude.go
+++ scad/linearextrude.go
@@ -4,7 +4,7 @@ import (
 	"github.com/micahkemp/scad/scad/internal"
 )
 
-func NewLinearExtrude(h float64, n string, c ...scadTemplate) scadTemplate {
+func NewLinearExtrude(n string, h float64, c ...scadTemplate) scadTemplate {
 	name, _ := internal.FirstNonEmptyName(n, "linear_extrude_component")
 	fields := internal.NewFields(map[string]string{
 		"h": internal.ShortFloat(h),
@@ -13,6 +13,6 @@ func NewLinearExtrude(h float64, n string, c ...scadTemplate) scadTemplate {
 	return newTemplate(name, internal.TransformationTemplate, "linear_extrude", fields, c...)
 }
 
-func (t scadTemplate) LinearExtrude(h float64, n string) scadTemplate {
-	return NewLinearExtrude(h, n, t)
+func (t scadTemplate) LinearExtrude(n string, h float64) scadTemplate {
+	return NewLinearExtrude(n, h, t)
 }
