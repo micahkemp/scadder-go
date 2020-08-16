143dfcabb012404179a9c6812288354a4c6b26e5
Micah Kemp <apple@micah.ws>
2020-08-16 14:54:11 -0500

    Name is the first parameter to NewMirror.

--- scad/mirror.go
+++ scad/mirror.go
@@ -2,7 +2,7 @@ package scad
 
 import "github.com/micahkemp/scad/scad/internal"
 
-func NewMirror(x, y, z float64, n string, c ...scadTemplate) scadTemplate {
+func NewMirror(n string, x, y, z float64, c ...scadTemplate) scadTemplate {
 	name, _ := internal.FirstNonEmptyName(n, "mirror_component")
 	fields := internal.NewFields(map[string]string{
 		"v": internal.ShortFloatList(x, y, z),
@@ -11,6 +11,6 @@ func NewMirror(x, y, z float64, n string, c ...scadTemplate) scadTemplate {
 	return newTemplate(name, internal.TransformationTemplate, "mirror", fields, c...)
 }
 
-func (t scadTemplate) Mirror(x, y, z float64, n string) scadTemplate {
-	return NewMirror(x, y, z, n, t)
+func (t scadTemplate) Mirror(n string, x, y, z float64) scadTemplate {
+	return NewMirror(n, x, y, z, t)
 }
