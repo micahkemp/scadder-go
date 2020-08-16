1b6e7e512673c4f387532b6587c3d790d5271058
Micah Kemp <apple@micah.ws>
2020-08-16 12:25:37 -0500

    Added related changes to previous commit, that were missed during git add.

--- scad/cube.go
+++ scad/cube.go
@@ -3,7 +3,7 @@ package scad
 import "github.com/micahkemp/scad/scad/internal"
 
 func NewCube(x, y, z float64, n string) scadTemplate {
-	name, _ := internal.FirstValidName(n, "cube_component")
+	name, _ := internal.FirstNonEmptyName(n, "cube_component")
 	fields := internal.NewFields(map[string]string{
 		"size": internal.ShortFloatList(x, y, z),
 	})
--- scad/cylinder.go
+++ scad/cylinder.go
@@ -6,10 +6,10 @@ import (
 )
 
 func NewCylinderByRadius(r, h float64, center bool, n string) scadTemplate {
-	name, _ := internal.FirstValidName(n, "cylinder_component")
+	name, _ := internal.FirstNonEmptyName(n, "cylinder_component")
 	fields := internal.NewFields(map[string]string{
-		"r": internal.ShortFloat(r),
-		"h": internal.ShortFloat(h),
+		"r":      internal.ShortFloat(r),
+		"h":      internal.ShortFloat(h),
 		"center": strconv.FormatBool(center),
 	})
 
