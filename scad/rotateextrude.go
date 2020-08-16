ad39081428d53dc301f2d6ff87418554d474167a
Micah Kemp <apple@micah.ws>
2020-08-16 14:57:21 -0500

    Name is the first parameter to NewRotateExtrude.

--- scad/rotateextrude.go
+++ scad/rotateextrude.go
@@ -4,7 +4,7 @@ import (
 	"github.com/micahkemp/scad/scad/internal"
 )
 
-func NewRotateExtrude(angle float64, n string, c ...scadTemplate) scadTemplate {
+func NewRotateExtrude(n string, angle float64, c ...scadTemplate) scadTemplate {
 	name, _ := internal.FirstNonEmptyName(n, "rotate_extrude_component")
 	fields := internal.NewFields(map[string]string{
 		"angle": internal.ShortFloat(angle),
@@ -13,6 +13,6 @@ func NewRotateExtrude(angle float64, n string, c ...scadTemplate) scadTemplate {
 	return newTemplate(name, internal.TransformationTemplate, "rotate_extrude", fields, c...)
 }
 
-func (t scadTemplate) RotateExtrude(angle float64, n string) scadTemplate {
-	return NewRotateExtrude(angle, n, t)
+func (t scadTemplate) RotateExtrude(n string, angle float64) scadTemplate {
+	return NewRotateExtrude(n, angle, t)
 }
