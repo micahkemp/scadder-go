2bcee1dc96a4c3f94ff9b840dd9486105a1627c2
Micah Kemp <apple@micah.ws>
2020-08-16 14:36:49 -0500

    Added center to Square.

--- scad/square.go
+++ scad/square.go
@@ -1,11 +1,15 @@
 package scad
 
-import "github.com/micahkemp/scad/scad/internal"
+import (
+	"github.com/micahkemp/scad/scad/internal"
+	"strconv"
+)
 
-func NewSquare(x, y float64, n string) scadTemplate {
+func NewSquare(x, y float64, center bool, n string) scadTemplate {
 	name, _ := internal.FirstNonEmptyName(n, "square_component")
 	fields := internal.NewFields(map[string]string{
-		"size": internal.ShortFloatList(x, y),
+		"size":   internal.ShortFloatList(x, y),
+		"center": strconv.FormatBool(center),
 	})
 
 	return newTemplate(name, internal.ShapeTemplate, "square", fields)
