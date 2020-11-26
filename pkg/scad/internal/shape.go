6933e17ac3b10c4de42decdb533e3ee95d4c6e47
Micah Kemp <apple@micah.ws>
2020-11-25 21:27:13 -0600

    Added pkg/scad/internal/shape and pkg/scad/internal/transformation from diagram_first.

---
+++ pkg/scad/internal/shape.go
@@ -0,0 +1,8 @@
+package internal
+
+const ShapeTemplate = `module {{ .Name.String }}() {
+	{{ .CallName }}({{ .Fields }});
+}
+// call module when loaded directly
+{{ .Name.String }}();
+`
---
+++ pkg/scad/internal/transformation.go
@@ -0,0 +1,13 @@
+package internal
+
+const TransformationTemplate = `{{ range .Children }}use <{{ .Name }}/{{ .Name.Filename }}>
+{{ end }}
+module {{ .Name.String }}() {
+	{{ .CallName }}({{ .Fields }}) { {{- range .Children }}
+		{{ .Name }}();
+	{{- end }}
+	}
+}
+// call module when loaded directly
+{{ .Name.String }}();
+`
