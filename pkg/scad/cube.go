7d6c2eeb905d80b8882685881e281e488bf3ba29
Micah Kemp <apple@micah.ws>
2020-11-25 22:10:08 -0600

    Added pkg/scad/cube, reworked to enable direct creation.

---
+++ pkg/scad/cube.go
@@ -0,0 +1,44 @@
+package scad
+
+import (
+	"github.com/micahkemp/scad/pkg/scad/internal"
+	"strconv"
+)
+
+type Cube struct {
+	Name string
+	X, Y, Z float64
+	Center bool
+	internal.ScadTemplate
+}
+
+var CubeExample = Cube{
+	Name: "cube_example",
+	X: 10,
+	Y: 20,
+	Z: 30,
+	Center: true,
+}
+
+func NewCube(name string, x, y, z float64, center bool) Cube {
+	cube := Cube{
+		Name: name,
+		X: x,
+		Y: y,
+		Z: z,
+		Center: center,
+	}
+	cube.Initialize()
+
+	return cube
+}
+
+func (c* Cube) Initialize() {
+	n, _ := internal.FirstNonEmptyName(c.Name, "cube_component")
+	fields := internal.NewFields(map[string]string{
+		"size":   internal.ShortFloatList(c.X, c.Y, c.Z),
+		"center": strconv.FormatBool(c.Center),
+	})
+
+	c.ScadTemplate = internal.NewTemplate(n, internal.ShapeTemplate, "cube", fields)
+}
---
+++ pkg/scad/cube_test.go
@@ -0,0 +1,43 @@
+package scad
+
+import (
+	"testing"
+)
+
+func TestCube(t *testing.T) {
+	tests := []struct {
+		n       string
+		x, y, z float64
+		center  bool
+		want    string
+	}{
+		{"", 1, 2, 3, false, `module cube_component() {
+	cube(center=false, size=[1, 2, 3]);
+}
+// call module when loaded directly
+cube_component();
+`},
+		{"my_cube", 1, 2, 3, true, `module my_cube() {
+	cube(center=true, size=[1, 2, 3]);
+}
+// call module when loaded directly
+my_cube();
+`},
+	}
+
+	for _, test := range tests {
+		c := NewCube(test.n, test.x, test.y, test.z, test.center)
+
+		if c.String() != test.want {
+			t.Errorf("NewCube(%s, %f, %f, %f, %t).String() = %s, want %s",
+				test.n,
+				test.x,
+				test.y,
+				test.z,
+				test.center,
+				c.String(),
+				test.want,
+			)
+		}
+	}
+}
