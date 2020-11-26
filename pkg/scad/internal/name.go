48cda88939d600754d06c33cf0c49c3a838bcf89
Micah Kemp <apple@micah.ws>
2020-11-25 21:20:02 -0600

    Forklifted internal/name from diagram_first.

---
+++ pkg/scad/internal/name.go
@@ -0,0 +1,61 @@
+package internal
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+)
+
+type Name string
+
+// start with letter, end with letter
+// digits and underscores permitted between
+const validPattern = "^[a-zA-Z][a-zA-Z0-9_]*[a-zA-Z0-9]$"
+
+// filename extension
+const extension = "scad"
+
+func valid(s string) bool {
+	return regexp.MustCompile(validPattern).MatchString(s)
+}
+
+func NewName(s string) (n Name, ok bool) {
+	if valid(s) {
+		n = Name(s)
+		ok = true
+	}
+
+	return
+}
+
+// returns
+//   name: first non-empty name passed
+//   ok:   the name's validity
+func FirstNonEmptyName(names ...string) (name string, ok bool) {
+	for _, name = range names {
+		if name != "" {
+			_, ok = NewName(name)
+			return
+		}
+	}
+
+	// clear name before returning with ok = false
+	name = ""
+	return
+}
+
+func (n Name) String() string {
+	return string(n)
+}
+
+func (n Name) DirPath(p string) string {
+	return filepath.Join(p, n.String())
+}
+
+func (n Name) Filename() string {
+	return fmt.Sprintf("%s.%s", n, extension)
+}
+
+func (n Name) FilePath(p string) string {
+	return filepath.Join(p, n.Filename())
+}
---
+++ pkg/scad/internal/name_test.go
@@ -0,0 +1,95 @@
+package internal
+
+import "testing"
+
+func TestValid(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"", false},
+		{"a", false},
+		{"0a", false},
+		{"a-a", false},
+		{"_a", false},
+		{"a_", false},
+		{"a a", false},
+		{"a0a", true},
+		{"aa", true},
+		{"a_a", true},
+		{"a0", true},
+	}
+
+	for _, test := range tests {
+		if got := valid(test.input); got != test.want {
+			t.Errorf("Valid(%q) = %v", test.input, got)
+		}
+	}
+}
+
+func TestFirstNonEmptyName(t *testing.T) {
+	tests := []struct {
+		choices  []string
+		wantName string
+		wantOk   bool
+	}{
+		{[]string{"", "a"}, "a", false},
+		{[]string{"", "a", "aa", "bb", "cc"}, "a", false},
+		{[]string{"", "aa", "bb", "cc"}, "aa", true},
+	}
+
+	for _, test := range tests {
+		if got, ok := FirstNonEmptyName(test.choices...); got != test.wantName || ok != test.wantOk {
+			t.Errorf("FirstNonEmptyName(%q) = (%q, %v), want (%q, %v)",
+				test.choices,
+				got,
+				ok,
+				test.wantName,
+				test.wantOk)
+		}
+	}
+}
+
+func TestFilenameFilePath(t *testing.T) {
+	tests := []struct {
+		name         string
+		path         string
+		wantDirPath  string
+		wantFilename string
+		wantFilePath string
+	}{
+		{"aa", ".", "aa", "aa.scad", "aa.scad"},
+		{"bb", "relative_output_path", "relative_output_path/bb", "bb.scad", "relative_output_path/bb.scad"},
+		{"cc", "/absolute_output_path", "/absolute_output_path/cc", "cc.scad", "/absolute_output_path/cc.scad"},
+	}
+
+	for _, test := range tests {
+		n, _ := NewName(test.name)
+		gotDirPath := n.DirPath(test.path)
+		gotFilename := n.Filename()
+		gotFilePath := n.FilePath(test.path)
+
+		if gotDirPath != test.wantDirPath {
+			t.Errorf("NewName(%q).DirPath(%q) = #{%q}, want %q",
+				test.name,
+				test.path,
+				gotDirPath,
+				test.wantDirPath)
+		}
+
+		if gotFilename != test.wantFilename {
+			t.Errorf("NewName(%q).Filename() = %q, want %q",
+				test.name,
+				gotFilename,
+				test.wantFilename)
+		}
+
+		if gotFilePath != test.wantFilePath {
+			t.Errorf("NewName(%q).FilePath(%q) = %q, want %q",
+				test.name,
+				test.path,
+				gotFilePath,
+				test.wantFilePath)
+		}
+	}
+}
