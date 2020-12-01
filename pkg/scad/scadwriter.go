8169a2dda636f98e362960716895bc937487dd39
Micah Kemp <apple@micah.ws>
2020-11-30 22:55:43 -0600

    Added writing of files.

--- pkg/scad/scadwriter.go
+++ pkg/scad/scadwriter.go
@@ -2,8 +2,9 @@ package scad
 
 import (
 	"bytes"
-	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	tt "text/template"
 )
 
@@ -25,10 +26,41 @@ func (t SCADWriter) content() string {
 	return buf.String()
 }
 
-func (t SCADWriter) WriteSCAD(path string) {
-	for _, child := range t.Children {
-		child.SCADWriter().WriteSCAD(t.PathFrom(path))
+func (t SCADWriter) rendered(path string) bool {
+	contents, err := ioutil.ReadFile(t.Name.SCADFilePath(path))
+
+	if err != nil {
+		// any error assumes not rendered
+		return false
 	}
 
-	fmt.Printf("%s - %s\n", t.Name.SCADFilePath(path), t.content())
+	return string(contents) == t.String()
+}
+
+func (t SCADWriter) WriteSCAD(path string) {
+	if err := os.MkdirAll(path, 0755); err != nil {
+		log.Fatalf("Unable to create directory: %s: %s", path, err)
+	}
+
+	// handle children first
+	for _, c := range t.Children {
+		c.SCADWriter().WriteSCAD(c.SCADWriter().Name.PathFrom(path))
+	}
+
+	if t.rendered(path) {
+		return
+	}
+
+	// OpenFile used specifically to enforce not overwriting an existing file, but instead failing
+	f, err := os.OpenFile(t.Name.SCADFilePath(path), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
+	// TODO this needs to be handled properly
+	if err != nil {
+		log.Fatalf("(scadTemplate) Render(%q): %s\n", path, err)
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(t.content()); err != nil {
+		log.Fatalf("(scadTemplate) Render(%q): %s\n", path, err)
+	}
 }
