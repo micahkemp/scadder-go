a450de6db049939bb8075cc0a4f5bff1704a07c1
Micah Kemp <apple@micah.ws>
2020-12-03 23:43:47 -0600

    Fixed dimple to not remove any of the sphere and offset only as appropriate for the depth specified.

--- examples/pkg/dice/dimple.go
+++ examples/pkg/dice/dimple.go
@@ -11,29 +11,17 @@ type Dimple struct {
 var ExampleDimple = Dimple{
 	Name:     "example_dimple",
 	Diameter: 10,
-	Depth:    8,
+	Depth:    3,
 }
 
 func (d Dimple) SCADWriter() scad.SCADWriter {
-	sphere := scad.Transformable(scad.Sphere{
+	return scad.Transformable(scad.Sphere{
 		Diameter: d.Diameter,
-	}).Translate(0, 0, d.Depth-d.Diameter)
-
-	subtractCube := scad.Transformable(scad.Cube{
-		X:      d.Diameter,
-		Y:      d.Diameter,
-		Z:      d.Diameter,
-		Center: true,
-	}).TranslateWithName("subtract_cube", 0, 0, -d.Diameter/2)
-
-	return scad.Difference{
-		Name: scad.Name{
+	}).TranslateWithName(
+		scad.Name{
 			Specified: d.Name,
 			Default:   "dimple",
 		}.String(),
-		Children: []scad.Module{
-			sphere,
-			subtractCube,
-		},
-	}.SCADWriter()
+		0, 0, d.Depth-d.Diameter/2,
+	).SCADWriter()
 }
