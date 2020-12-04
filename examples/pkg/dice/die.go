9ddde2055614d48a94691f6772c4ddb786a181bb
Micah Kemp <apple@micah.ws>
2020-12-03 23:55:18 -0600

    Subtract side dimples from die.

--- examples/pkg/dice/die.go
+++ examples/pkg/dice/die.go
@@ -3,24 +3,38 @@ package dice
 import "github.com/micahkemp/scad/pkg/scad"
 
 type Die struct {
-	Name string
-	Size float64
+	Name           string
+	Size           float64
+	DimpleDiameter float64
+	DimpleDepth    float64
 }
 
 var ExampleDie = Die{
-	Name: "example_die",
-	Size: 10,
+	Name:           "example_die",
+	Size:           50,
+	DimpleDiameter: 10,
+	DimpleDepth:    8,
 }
 
 func (d Die) SCADWriter() scad.SCADWriter {
-	return scad.Cube{
-		Name: scad.Name{
-			Specified: d.Name,
-			Default:   "die",
-		}.String(),
+	dimples := SideDimples{
+		SideLength:     d.Size,
+		DimpleDiameter: d.DimpleDiameter,
+		DimpleDepth:    d.DimpleDepth,
+	}
+
+	cube := scad.Cube{
 		X:      d.Size,
 		Y:      d.Size,
 		Z:      d.Size,
-		Center: false,
-	}.SCADWriter()
+		Center: true,
+	}
+
+	return scad.Transformable(cube).SubtractWithName(
+		scad.Name{
+			Specified: d.Name,
+			Default:   "die",
+		}.String(),
+		dimples,
+	).SCADWriter()
 }
